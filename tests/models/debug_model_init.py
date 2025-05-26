e396891f24033dd69308746241ad413df4dd9111
jeromeku <jerome.ku@gmail.com>
2025-05-26 15:25:50 -0700

    add model init trace script

---
+++ tests/models/debug_model_init.py
@@ -0,0 +1,179 @@
+# SPDX-License-Identifier: Apache-2.0
+import os
+
+os.environ["VLLM_ENABLE_V1_MULTIPROCESSING"] = "0"
+os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
+os.environ["VLLM_LOGGING_LEVEL"] = "DEBUG"
+os.environ["VLLM_TRACE_FUNCTION"] = "0"
+
+from collections.abc import Iterable
+from contextlib import ExitStack
+from unittest.mock import patch
+
+import torch
+from transformers import PretrainedConfig
+
+from vllm import LLM
+from vllm.engine.llm_engine import LLMEngine as V0LLMEngine
+from vllm.logger import logger
+from vllm.model_executor.models.qwen3 import AutoWeightsLoader, Qwen3ForCausalLM
+from vllm.utils import GiB_bytes
+from vllm.v1.core.kv_cache_utils import get_kv_cache_config
+from vllm.v1.engine.core import EngineCore as V1EngineCore
+
+from .registry import QWEN3_EXAMPLE_MODELS
+
+NUM_LAYERS_TO_LOAD = 1
+
+
+def vllm_trace_enabled():
+    return os.environ.get("VLLM_TRACE_FUNCTION", 0) == "1"
+
+
+def get_layer_prefixes_to_exclude(
+    model_id: str, num_layers_to_load: int = NUM_LAYERS_TO_LOAD
+):
+    from transformers import AutoConfig
+
+    config = AutoConfig.from_pretrained(model_id)
+    num_layers = config.num_hidden_layers
+
+    return [f"layers.{i}." for i in range(num_layers_to_load, num_layers)]
+
+
+def patched_loader(model_id: str, num_layers_to_load: int = NUM_LAYERS_TO_LOAD):
+    layer_pats_to_exclude = get_layer_prefixes_to_exclude(
+        model_id, num_layers_to_load
+    )
+
+    def load_weights(
+        self, weights: Iterable[tuple[str, torch.Tensor]]
+    ) -> set[str]:
+        skip_substrs = layer_pats_to_exclude
+        if self.config.tie_word_embeddings:
+            skip_prefixes = ["lm_head."]
+        logger.debug(
+            f"Setting skip prefixes for {model_id} to {skip_prefixes}, substrs {skip_substrs}"
+        )
+        loader = AutoWeightsLoader(
+            self, skip_prefixes=skip_prefixes, skip_substrs=skip_substrs
+        )
+        return loader.load_weights(weights)
+
+    return load_weights
+
+
+def _initialize_kv_caches_v1(self, vllm_config):
+    logger.debug("Overriding v1 kvcache...")
+    kv_cache_specs = self.model_executor.get_kv_cache_specs()
+    scheduler_kv_cache_config = get_kv_cache_config(
+        vllm_config,
+        kv_cache_specs[0],
+        10 * GiB_bytes,
+    )
+    # gpu_blocks (> 0), cpu_blocks, scheduler_kv_cache_config
+    return 1, 0, scheduler_kv_cache_config
+
+
+# Avoid calling model.forward()
+def _initialize_kv_caches_v0(self) -> None:
+    logger.debug("Overriding v0 kvcache...")
+
+    self.cache_config.num_gpu_blocks = 0
+    self.cache_config.num_cpu_blocks = 0
+
+
+# Avoid OOM and reduce initialization time by only using 1 layer
+def hf_overrides(
+    model_info,
+) -> PretrainedConfig:
+    def _inner(hf_config: PretrainedConfig):
+        hf_config.update(model_info.hf_overrides)
+        text_config = hf_config.get_text_config()
+
+        text_config.update(
+            {
+                "num_layers": 1,
+                "num_hidden_layers": 1,
+                "num_experts": 2,
+                "num_experts_per_tok": 2,
+                "num_local_experts": 2,
+            }
+        )
+
+        if hasattr(hf_config, "vision_config"):
+            hf_config.vision_config.update(
+                {
+                    "num_layers": 1,
+                    "num_hidden_layers": 1,
+                }
+            )
+
+        return hf_config
+
+    _inner.__name__ = "debugging_config"
+
+    return _inner
+
+
+def test_qwen3(model_arch: str, num_layers_to_load: int = 1):
+    from vllm.debugging import get_tracer
+
+    model_info = QWEN3_EXAMPLE_MODELS.get_hf_info(model_arch)
+    model_info.check_available_online(on_fail="skip")
+    model_info.check_transformers_version(on_fail="skip")
+
+    Qwen3ForCausalLM.load_weights = patched_loader(
+        model_info.default, num_layers_to_load=num_layers_to_load
+    )
+
+    model_id = model_arch.split("/")[-1]
+    print(f"Testing model {model_id}")
+
+    if not vllm_trace_enabled():
+        tracer = get_tracer()
+        tracer.output_file = f"{model_id}.init.json"
+
+    with ExitStack() as stack:
+        stack.enter_context(
+            patch.object(
+                V0LLMEngine, "_initialize_kv_caches", _initialize_kv_caches_v0
+            )
+        )
+        stack.enter_context(
+            patch.object(
+                V1EngineCore, "_initialize_kv_caches", _initialize_kv_caches_v1
+            )
+        )
+        if not vllm_trace_enabled():
+            stack.enter_context(tracer)
+        # ignore_patterns = get_layer_prefixes_to_exclude(model_info.default)
+        llm = LLM(
+            model_info.default,
+            tokenizer=model_info.tokenizer,
+            tokenizer_mode=model_info.tokenizer_mode,
+            speculative_config={
+                "model": model_info.speculative_model,
+                "num_speculative_tokens": 1,
+            }
+            if model_info.speculative_model
+            else None,
+            trust_remote_code=model_info.trust_remote_code,
+            max_model_len=model_info.max_model_len,
+            # load_format="dummy" if load_dummy else None,
+            hf_overrides=hf_overrides(model_info),
+            # ignore_patterns=ignore_patterns,
+        )
+
+
+if __name__ == "__main__":
+    vllm_env = "\n".join(
+        f"{k}={v}" for k, v in os.environ.items() if "VLLM" in k
+    )
+    logger.debug(f"Running with VLLM env: {vllm_env}")
+
+    model_archs = QWEN3_EXAMPLE_MODELS.get_supported_archs()
+    for model_arch in model_archs:
+        print(f"Testing model arch: {model_arch}")
+        test_qwen3(model_arch=model_arch)
+        break
