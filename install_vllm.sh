532457ac99a44047db5521fde705b3907566808b
jeromeku <jerome.ku@gmail.com>
2025-08-19 16:47:04 +0000

    update install script

--- install_vllm.sh
+++ install_vllm.sh
@@ -1,3 +1,3 @@
-INSTALL_CMD="VLLM_USE_PRECOMPILED=1 uv pip install --editable . -v"
+INSTALL_CMD="VLLM_USE_PRECOMPILED=1 uv pip install --editable . --torch-backend=cu128 -v"
 echo "Installing vllm editable..." && eval ${INSTALL_CMD} 2>&1 | tee _vllm.build.log
 python -m patch_vllm_logger
